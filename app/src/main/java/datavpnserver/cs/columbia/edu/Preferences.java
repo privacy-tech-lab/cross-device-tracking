29eb8880225743c4cb77948f9b038ef193c6dfee
SebastianZimmeck <sebastian@sebastianzimmeck.de>
2015-03-12 12:40:30 -0400

    Major revisions: Automatic transfer of browser history to server and user sign in to app

---
+++ app/src/main/java/datavpnserver/cs/columbia/edu/Preferences.java
@@ -0,0 +1,24 @@
+package datavpnserver.cs.columbia.edu;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class Preferences extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    // save user ID
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences); // deprecated, but necessary for older APIs
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    // upon storing user ID stop preferences activity and return to main activity
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        finish();
+    }
+
+}
